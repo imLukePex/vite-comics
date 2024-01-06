b934d6cd20ac63a4658fd2fd42249d949a092843
imLukePex <luca.pessione@gmail.com>
2024-01-06 21:55:02 +0100

    AppHeader commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,95 +1,137 @@
 <script>
 export default {
     name: "AppHeader",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    status: false,
+                },
+                {
+                    text: "COMICS",
+                    status: true,
+                },
+                {
+                    text: "MOVIES",
+                    status: false,
+                },
+                {
+                    text: "TV",
+                    status: false,
+                },
+                {
+                    text: "GAMES",
+                    status: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    status: false,
+                },
+                {
+                    text: "VIDEOS",
+                    status: false,
+                },
+                {
+                    text: "FANS",
+                    status: false,
+                },
+                {
+                    text: "NEWS",
+                    status: false,
+                },
+                {
+                    text: "SHOP",
+                    status: false,
+                },
+
+            ]
+        }
+    }, methods: {
+        activeLink(i) {
+            if (this.links[i].status === false) {
+                this.links[i].status = true;
+            } else {
+                this.links[i].status = false;
+            }
+        }
+    }
 }
 </script>
 
 <template>
     <header>
-        <!-- logo principale -->
-        <div class="logo-box">
-            <a href="#">
-                <img src="/dc-logo.png" alt="dc-logo">
-            </a>
+        <div class="container">
+            <!-- logo principale -->
+            <div class="logo-box">
+                <a href="#">
+                    <img src="/dc-logo.png" alt="dc-logo">
+                </a>
+            </div>
+
+            <nav>
+                <ul>
+                    <li v-for="(link, i) in links" :key="i"><a href="#" :class="link.status ? 'active' : ''"
+                            @click="activeLink(i)">{{ link.text
+                            }}</a></li>
+                </ul>
+            </nav>
         </div>
-
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">CHARACTERS</a>
-                </li>
-
-                <li>
-                    <a href="#" class="active">COMICS</a>
-                </li>
-
-                <li>
-                    <a href="#">MOVIES</a>
-                </li>
-
-                <li>
-                    <a href="#">TV</a>
-                </li>
-
-                <li>
-                    <a href="#">GAMES</a>
-                </li>
-
-                <li>
-                    <a href="#">COLLECTIBLES</a>
-                </li>
-
-                <li>
-                    <a href="#">VIDEOS</a>
-                </li>
-
-                <li>
-                    <a href="#">FANS</a>
-                </li>
-
-                <li>
-                    <a href="#">NEWS</a>
-                </li>
-
-                <li>
-                    <a href="#">SHOP</a>
-                </li>
-            </ul>
-        </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 @use '../styles/partials/mixins' as *;
+@use '../styles/general.scss' as *;
 
 header {
-    height: 140px;
-    @include center('horizontal');
+    height: 120px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 500;
     background: lightblue;
-    justify-content: space-between;
 
-    ul {
-        list-style: none;
+    .container {
         @include center();
+        height: 100%;
+        justify-content: space-between;
 
-        li a {
-            display: inline-block;
-            padding: 1rem;
-            text-decoration: none;
-            font-weight: 600;
-            transition: background 0.3s;
+        .logo-box {
+            width: 80px;
+        }
 
-            &.active,
-            &:hover {
-                background: lighten($primary, 70%);
+        ul {
+            list-style: none;
+            @include center();
+            justify-content: space-between;
+
+            li {
+                padding-left: 25px;
+
+                a {
+                    text-decoration: none;
+                    color: #000;
+                    font-size: 15px;
+                    padding: 43px 0;
+
+                    &.active,
+                    &:hover {
+                        color: $primary;
+                    }
+
+                    &.active {
+                        border-bottom: 5px solid $primary;
+                    }
+
+                }
             }
+
         }
     }
 }
-
-
-
 
 // Qui ci andrà l'scss relativo al solo componente
 </style>
